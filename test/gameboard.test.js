83c62959a7d3c2530b095d7ccc7e52025180efdb
ls1955 <xxlsxx1955@gmail.com>
2023-11-01 22:04:34 +0800

    Include Ship class and its unit test

---
+++ test/gameboard.test.js
@@ -0,0 +1,59 @@
+import { Gameboard } from "../lib/gameboard";
+import { Ship } from "../lib/ship";
+
+test("has 10 rows, each row has 10 columns", () => {
+    let gameboard = new Gameboard();
+    expect(gameboard.board.length).toEqual(10);
+});
+
+test("has 0 missed shots at the beginning", () => {
+    let gameboard = new Gameboard();
+    expect(gameboard.missedShots).toEqual(0);
+});
+
+test("could place a ship on the board", () => {
+    let gameboard = new Gameboard();
+    let ship = new Ship({ length: 1 });
+    gameboard.place({ ship, x: 0, y: 0 });
+
+    expect(gameboard.board[0][0]).toBe(ship);
+    for (let i = 0; i < gameboard.length; i++) {
+        for (let j = 0; j < GamepadButton[0].length; j++)
+            if (i === 0 && j === 0) continue;
+
+        expect(gameboard.board[i][j]).not.toBe(ship);
+    }
+});
+
+test("could receive attack and transfer the hit to the ship when there is ship", () => {
+    let gameboard = new Gameboard();
+    let ship = new Ship({ length: 1 });
+    gameboard.place({ ship, x: 0, y: 0 });
+    gameboard.receiveAttack({ x: 0, y: 0 });
+
+    expect(gameboard.board[0][0].receivedHitAmount).toEqual(1);
+    expect(gameboard.missedShots).toEqual(0);
+});
+
+test("increment missedShots if receive attack on an empty coordinate", () => {
+    let gameboard = new Gameboard();
+    gameboard.receiveAttack({ x: 0, y: 0 });
+
+    expect(gameboard.missedShots).toEqual(1);
+});
+
+test("return true if receive attack could transfer the hit to the ship", () => {
+    let gameboard = new Gameboard();
+    let ship = new Ship({ length: 1 });
+    gameboard.place({ ship, x: 0, y: 0 });
+    let result = gameboard.receiveAttack({ x: 0, y: 0 });
+
+    expect(result).toBeTruthy();
+});
+
+test("return false if receive attack on an empty coordinate", () => {
+    let gameboard = new Gameboard();
+    let result = gameboard.receiveAttack({ x: 0, y: 0 });
+
+    expect(result).toBeFalsy();
+});
