22737f9540b910f2514a19c8010cbff6899255f1
ls1955 <xxlsxx1955@gmail.com>
2023-11-04 21:50:29 +0800

    Include Player class and tests

---
+++ test/player.test.js
@@ -0,0 +1,23 @@
+import { Player } from "../lib/player";
+
+const playerBoard = jest.createMockFromModule("../lib/gameboard");
+const enemyBoard = jest.createMockFromModule("../lib/gameboard");
+playerBoard.place = jest.fn();
+enemyBoard.receiveAttack = jest.fn();
+
+test("send a message to own board when placing ship", () => {
+    let ship = "mock ship";
+    let player = new Player({ ownBoard: playerBoard });
+    player.place({ ship, x: -1, y: -1 });
+
+    expect(playerBoard.place).toHaveBeenCalled();
+    expect(playerBoard.place.mock.calls).toHaveLength(1);
+});
+
+test("send a message to enemy board when attacking", () => {
+    let player = new Player({ enemyBoard });
+    player.attack({ x: -1, y: -1 });
+
+    expect(enemyBoard.receiveAttack).toHaveBeenCalled();
+    expect(enemyBoard.receiveAttack.mock.calls).toHaveLength(1);
+});
