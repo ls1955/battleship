e454e9891f42cbe4d1668a5c7ab9878502e36e19
ls1955 <xxlsxx1955@gmail.com>
2023-11-14 20:48:44 +0800

    Include the initial setup

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-import { DOMInitializer } from "./lib/dom_initializer.js";
+import { BattleShipController } from "./lib/battleship_controller.js";
+import { Gameboard } from "./lib/gameboard.js";
+import { Player } from "./lib/player.js";
+import { ComputerPlayer } from "./lib/computer_player.js";
 
-new DOMInitializer().initializeElements()
+let humanBoard = new Gameboard();
+let compBoard = new Gameboard();
+let humanPlayer = new Player({ ownBoard: humanBoard, enemyBoard: compBoard });
+let compPlayer = new ComputerPlayer({
+    ownBoard: compBoard,
+    enemyBoard: humanBoard,
+});
+let controller = new BattleShipController({
+    humanPlayer,
+    compPlayer,
+    humanBoard,
+    compBoard,
+});
+
+controller.initialize();
