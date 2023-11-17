9ae5b6e3340c2cff358a9241f3c18abe4b8ced14
ls1955 <xxlsxx1955@gmail.com>
2023-11-17 21:19:22 +0800

    Remove Player class
    When a class only purpose is redirecting the same
    parameters into other object, perhaps its time and
    wonder if these kind of class really need to exist
    or not.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import { BattleShipController } from "./lib/battleship_controller.js";
 import { Gameboard } from "./lib/gameboard.js";
-import { Player } from "./lib/player.js";
 import { ComputerPlayer } from "./lib/computer_player.js";
 
 let humanBoard = new Gameboard();
 let compBoard = new Gameboard();
-let humanPlayer = new Player({ ownBoard: humanBoard, enemyBoard: compBoard });
 let compPlayer = new ComputerPlayer({
     ownBoard: compBoard,
     enemyBoard: humanBoard,
 });
 let controller = new BattleShipController({
-    humanPlayer,
     compPlayer,
     humanBoard,
     compBoard,
