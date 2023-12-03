2a4ffdbb362e6e899c9b96a720d69325f77f7dfc
ls1955 <xxlsxx1955@gmail.com>
2023-12-03 18:39:11 +0800

    Set preview ship event for human board
    Include .preview-invalid rule in style.css

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import { Gameboard } from "./src/gameboard.js";
 import { BoardDOMFiller } from "./src/board_dom_filler.js";
+import { BoardEventSetter } from "./src/board_event_setter.js";
+import { Shipyard } from "./src/shipyard.js";
+import { Ship } from "./src/ship.js";
+
+// Hardcode the ship lengths for both player.
+const shipLengths = [5, 4, 4, 3, 2, 2];
 
 let humanBrd = new Gameboard();
 let compBrd = new Gameboard();
 humanBrd.dom = document.querySelector(".human-board");
 compBrd.dom = document.querySelector(".computer-board");
 
+let ships = shipLengths.map((length) => new Ship({ length }));
+let humanShipyard = new Shipyard({ ships });
+
+// Fills up the board with rows and columns in DOM...
 [humanBrd, compBrd].forEach((board) => {
     new BoardDOMFiller().fill({
         board,
@@ -13,3 +23,9 @@ compBrd.dom = document.querySelector(".computer-board");
         height: Gameboard.Height,
     });
 });
+
+// Setup various board events...
+let setter = new BoardEventSetter();
+setter.addPreviewShipEvent({ board: humanBrd, shipyard: humanShipyard });
+// TODO: Setup placeShipEvent, computerPlaceShipEvent...
+// TODO: Perhaps have a BattleShip class that keep track of various game status?
