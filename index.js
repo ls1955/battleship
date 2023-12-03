5535414cd8ea65d247a11c40e39b210b9e1a9901
ls1955 <xxlsxx1955@gmail.com>
2023-12-03 21:41:19 +0800

    Let computer player place their ships

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
+import { ComputerPlayer } from "./src/computer_player.js";
+import { ComputerController } from "./src/computer_controller.js";
 import { Gameboard } from "./src/gameboard.js";
 import { BoardDOMFiller } from "./src/board_dom_filler.js";
 import { BoardEventSetter } from "./src/board_event_setter.js";
 import { Shipyard } from "./src/shipyard.js";
 import { Ship } from "./src/ship.js";
 
-// Hardcode the ship lengths for both player.
+// Hardcode the ship lengths for both player, modify this to your desired lengths.
 const shipLengths = [5, 4, 4, 3, 2, 2];
 
 let humanBrd = new Gameboard();
 let compBrd = new Gameboard();
 humanBrd.dom = document.querySelector(".human-board");
 compBrd.dom = document.querySelector(".computer-board");
-
-let ships = shipLengths.map((length) => new Ship({ length }));
-let humanShipyard = new Shipyard({ ships });
 
 // Fills up the board with rows and columns in DOM...
 [humanBrd, compBrd].forEach((board) => {
@@ -24,9 +23,22 @@ let humanShipyard = new Shipyard({ ships });
     });
 });
 
-// Setup various board events...
+let ships = shipLengths.map((length) => new Ship({ length }));
+let humanShipyard = new Shipyard({ ships });
+
+// Setup various board events (human side)...
 let setter = new BoardEventSetter();
 setter.addPreviewShipEvent({ board: humanBrd, shipyard: humanShipyard });
 setter.addPlaceShipEvent({ board: humanBrd, shipyard: humanShipyard });
-// TODO: Setup computerPlaceShipEvent...
+
+// Computer player placing their ships...
+let comp = new ComputerPlayer({
+    boardWidth: Gameboard.Width,
+    boardHeight: Gameboard.Height,
+});
+let compShipyard = new Shipyard({ ships });
+let compController = new ComputerController({ computerPlayer: comp });
+compController.placeShips({ board: compBrd, shipyard: compShipyard });
+
+// TODO: Add human attack event
 // TODO: Perhaps have a BattleShip class that keep track of various game status?
