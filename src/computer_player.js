0e4ae9015460a9d16e6384d27aeec6995446b213
ls1955 <xxlsxx1955@gmail.com>
2023-12-03 16:32:12 +0800

    Remove ownBoard and enemyBoard param
    Because:
    It currently does not need them to do its thing.
    Might change later on...

--- src/computer_player.js
+++ src/computer_player.js
@@ -1,21 +1,18 @@
 export class ComputerPlayer {
-    ownBoard = null;
-    enemyBoard = null;
-
-    constructor({ ownBoard, enemyBoard }) {
-        this.ownBoard = ownBoard;
-        // CHECK: Looks like we don't need the enemyBoard at all
-        this.enemyBoard = enemyBoard;
+    constructor({ boardWidth, boardHeight }) {
         this.atkCoordinates = [];
 
-        for (let y = 0; y < this.ownBoard.grid.length; y++) {
-            for (let x = 0; x < this.ownBoard.grid[0].length; x++) {
+        for (let y = 0; y < boardHeight; y++) {
+            for (let x = 0; x < boardWidth; x++) {
                 this.atkCoordinates.push([x, y]);
             }
         }
     }
 
-    // Place the ships randomly on the board
+    // Places the ships randomly on the board.
+    // Regarding current implementation, it choose a row and a valid column randomly.
+    // Since it currently will no repeat the row (even it there is enough space to fit
+    // another ship), probably need to change the implementation later on...
     place({ ships, board }) {
         const yCands = [0, 1, 2, 3, 4, 5, 7, 8, 9];
 
@@ -29,13 +26,14 @@ export class ComputerPlayer {
         });
     }
 
-    // Return a random coordinate in [x, y] form, will never repeat chosen coordinate
+    // Returns a random coordinate in [x, y] form, will never repeat chosen coordinate.
     nextAtkCoordinate() {
         let randomIndex = this.randomIndex(this.atkCoordinates);
         return this.atkCoordinates.splice(randomIndex, 1)[0];
     }
 
-    randomIndex(array) {
-        return Math.floor(Math.random() * array.length);
+    // Returns a random index from *ary*, should be use internally.
+    randomIndex(ary) {
+        return Math.floor(Math.random() * ary.length);
     }
 }
--- test/computer_player.test.js
+++ test/computer_player.test.js
@@ -1,29 +1,19 @@
 import { ComputerPlayer } from "../src/computer_player";
-import { Gameboard } from "../src/gameboard";
-
-const ownBoard = new Gameboard();
-const enemyBoard = jest.createMockFromModule("../src/gameboard");
-enemyBoard.receiveAttack = jest.fn();
 
 describe("ComputerPlayer", () => {
     describe("#nextAtkCoordinate", () => {
         it("does not repeat chosen attack coordinate", () => {
-            let colSize = 10;
-            let rowSize = 10;
-
-            let comp = new ComputerPlayer({
-                ownBoard,
-                enemyBoard,
-            });
+            let boardWidth = 10;
+            let boardHeight = 10;
+            let player = new ComputerPlayer({ boardWidth, boardHeight });
             let coordinates = new Set();
 
-            for (let i = 0; i < colSize; i++) {
-                for (let j = 0; j < rowSize; j++) {
-                    coordinates.add(comp.nextAtkCoordinate());
+            for (let y = 0; y < boardHeight; y++) {
+                for (let x = 0; x < boardWidth; x++) {
+                    coordinates.add(player.nextAtkCoordinate());
                 }
             }
-
-            expect(coordinates.size).toEqual(colSize * rowSize);
+            expect(coordinates.size).toEqual(boardWidth * boardHeight);
         });
     });
 });
