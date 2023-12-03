dd4ac75579e56e02a323b34b8a6ed3858db1510d
ls1955 <xxlsxx1955@gmail.com>
2023-12-03 21:34:13 +0800

    Include updateShipCSSKlass util function

--- src/dom_util.js
+++ src/dom_util.js
@@ -1,9 +1,11 @@
+import { Ship } from "./ship.js"
+
 // Returns columns (DOMs) inside the board (DOM) that are occupy by ship, if they exist.
 function getColumns({ board, ship, x, y }) {
     const result = [];
 
     for (let offset = 0; offset < ship.length; offset++) {
-        let c = getColumn({board, x: x + offset, y})
+        let c = getColumn({ board, x: x + offset, y });
         if (c != null) result.push(c);
     }
 
@@ -16,4 +18,17 @@ function getColumn({ board, x, y }) {
     return board.dom.querySelector(`.column[data-x="${x}"][data-y="${y}"]`);
 }
 
-export { getColumns, getColumn }
+// Adds .ship CSS class if corresponding column in board is a ship, else removes it.
+function updateShipCSSKlass({ board }) {
+    board.grid.forEach((row, y) => {
+        row.forEach((col, x) => {
+            let c = getColumn({ board, x, y });
+
+            col instanceof Ship
+                ? c.classList.add("ship")
+                : c.classList.remove("ship");
+        });
+    });
+}
+
+export { getColumns, getColumn, updateShipCSSKlass };
