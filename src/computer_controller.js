83841206b2374fd29b2b6e3b5b7bc7f17324662f
ls1955 <xxlsxx1955@gmail.com>
2023-12-04 14:53:03 +0800

    Include #attack

--- src/computer_controller.js
+++ src/computer_controller.js
@@ -11,4 +11,17 @@ export class ComputerController {
         this.computerPlayer.placeShips({ board });
         Util.updateShipCSS({ board });
     }
+
+    // Attack the *board* by getting coordinate from computer player.
+    // Update column's CSS after the attack.
+    attack({ board }) {
+        let [x, y] = this.computerPlayer.nextAtkCoordinate();
+
+        let column = Util.getColumn({ board, x, y });
+        if (board.receiveAttack({ x, y })) {
+            Util.setCSSKlasses({ column, adds: ["hit"], removes: [] });
+        } else {
+            Util.setCSSKlasses({ column, adds: ["miss"], removes: [] });
+        }
+    }
 }
