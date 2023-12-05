88fe160ebf7de0a8e678734e392df2cd7f4ba68e
ls1955 <xxlsxx1955@gmail.com>
2023-12-05 21:48:47 +0800

    Create dist/index.html from src/index.html
    This commit:
    * Move index.html -> src/index.html
    * Config HtmlWebpackPlugin to use src/index.html as template
    * Remove redundant index.js import in src/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "index.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Battleship",
+            template: "./src/index.html",
+        }),
+    ],
     mode: "production",
     module: {
         rules: [
